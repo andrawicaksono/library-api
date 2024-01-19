fbe93c2257ff263f8c6ea53f5929f77d8666d732
Andra Wicaksono <andrawicaksono14@gmail.com>
2024-01-19 17:56:27 +0700

    feat: add pagination & search

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 
 @Controller('books')
 export class BookController {
@@ -11,8 +12,11 @@ export class BookController {
     ) {}
 
     @Get()
-    async getAllBooks(): Promise<Book[]> {
-        return this.bookService.findAll();
+    async getAllBooks(
+        @Query()
+        query: ExpressQuery
+    ): Promise<Book[]> {
+        return this.bookService.findAll(query);
     }
 
     @Post()
